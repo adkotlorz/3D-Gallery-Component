8c08e1f8ecf8ef050f88a8a7b0e213856f07cfa5
adkotlorz <ad.kotlorz@gmail.com>
2025-06-08 11:54:31 +0200

    added const, build scripts and texture loader

---
+++ constants/index.js
@@ -0,0 +1,28 @@
+const images = [
+	'/socrates.jpg',
+	'/stars.jpg',
+	'/wave.jpg',
+	'/spring.jpg',
+	'/mountain.jpg',
+	'/sunday.jpg'
+];
+
+const titles = [
+	'The Death of Socrates',
+	'Starry Night',
+	'The Great Wave off Kanagawa',
+	'Effect of Spring, Giverny',
+	'Mount Corcoran',
+	'A Sunday on La Grande Jatte'
+];
+
+const artists = [
+	'Jacques-Louis David',
+	'Vincent Van Gogh',
+	'Katsushika Hokusai',
+	'Claude Monet',
+	'Albert Bierstadt',
+	'George Seurat'
+];
+
+export { images, titles, artists };
--- main.js
+++ main.js
@@ -1,24 +1,50 @@
 import * as THREE from 'three';
+import { images } from "./constants";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const textureLoader = new THREE.TextureLoader();
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const rootNode = new THREE.Object3D();
+scene.add( rootNode );
 
-camera.position.z = 5;
+let count = 6;
+for (let i = 0; i < count; i++) {
+	const texture = textureLoader.load(
+		images[i],
+		() => console.log(`Loaded: ${images[i]}`),
+		undefined,
+		(err) => console.error(`Error while loading: ${images[i]}`, err)
+	);
+	texture.colorSpace = THREE.SRGBColorSpace;
+
+
+	const baseNode = new THREE.Object3D();
+	baseNode.rotation.y = i * (2 * Math.PI / count);
+	rootNode.add( baseNode );
+
+	const artwork = new THREE.Mesh(
+		new THREE.BoxGeometry( 3, 2, 0.1 ),
+		new THREE.MeshBasicMaterial({ map: texture })
+	);
+	artwork.position.z = -4;
+
+	baseNode.add(artwork);
+}
 
 function animate() {
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
+	rootNode.rotation.y += 0.002;
 	renderer.render( scene, camera );
 }
+
+window.addEventListener('resize', () => {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+});
